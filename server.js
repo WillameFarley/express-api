f5febcbb1e3b200b5dab41c31684a250a7c29e96
Willame Farley <willamefarley@gmail.com>
2022-01-27 20:14:59 -0300

    Atualização fonte server.js

--- server.js
+++ server.js
@@ -7,48 +7,48 @@ app.use(express.json());
 let usuarios = [];
 
 app.get('/usuarios', (req, res) => {
-    return res.json(usuarios);
+  return res.json(usuarios);
 });
 
 app.post('/usuarios', (req, res) => {
-    const { nome, sobrenome } = req.body;
+  const { nome, sobrenome } = req.body;
 
-    const usuario = {
-        nome,
-        sobrenome
-    }
+  const usuario = {
+    nome,
+    sobrenome
+  };
 
-    usuarios.push(usuario);
+  usuarios.push(usuario);
 
-    return res.json(usuario);
+  return res.json(usuarios);
 });
 
-app.patch('/usuarios/:nome', (req, res)=> {
-    const { nome } = req.params;
+app.patch('/usuarios/:nome', (req, res) => {
+  const { nome } = req.params;
 
-    const { nome: novoNome, sobrenome: novoSobrenome } = req.body;
+  const { nome: novoNome, sobrenome: novoSobrenome } = req.body;
 
-    let usuario = usuarios.find((usu) => usu.nome === nome);
+  let usuario = usuarios.find((usu) => usu.nome === nome);
   
-    if (novoNome) {
-        usuario.nome = novoNome;
-    }
-    if (novoSobrenome) {
-        usuario.sobrenome = novoSobrenome;
-    }
+  if (novoNome) {
+    usuario.nome = novoNome;
+  }
+  if (novoSobrenome) {
+    usuario.sobrenome = novoSobrenome;
+  }
 
-    res.json(usuarios);
+  res.json(usuarios);
 });
 
 app.delete('/usuarios/:nome', (req, res) => {
-    const { nome } = req.params;
-  
-    const novaLista = usuarios.filter((usu) => usu.nome !== nome);
-    usuarios = novaLista;
-  
-    res.json(usuarios);
-});
-  
+  const { nome } = req.params;
+
+  const novaLista = usuarios.filter((usu) => usu.nome !== nome);
+  usuarios = novaLista;
+
+  res.json(usuarios);
+})
+
 app.listen(3000, () => {
-    console.log('Servidor aberto na porta 3000');
+  console.log('Servidor aberto na porta 3000');
 });
